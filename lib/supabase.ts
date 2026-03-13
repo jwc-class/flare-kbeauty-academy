22e9743f98d49c905251bcf878406a926a0e43d6
jwc-class <jwchoi@classaround.co.kr>
2026-03-13 14:00:08 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.https://mbccumhbcjfuufaucmjh.supabase.co!,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iY2N1bWhiY2pmdXVmYXVjbWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMjA4MjAsImV4cCI6MjA4ODc5NjgyMH0.FNImylOr-ytfBSNZFFRSXjnF04Ph2SQGJfV3_NLbwfo!
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
